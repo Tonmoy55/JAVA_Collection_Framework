d8b2e94a3930861173200ddbf530222d0e7904c2
tonmoy55 <nsu.tonmoy@gmail.com>
2022-12-19 20:06:06 +0600

    Test Commit

--- src/Collection_Interface/List_Interface/ArrayList_Class/ArrayList_Class.java
+++ src/Collection_Interface/List_Interface/ArrayList_Class/ArrayList_Class.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * 2nd commit
+ */
 public class ArrayList_Class {
     public static void main(String[] args) {
 
